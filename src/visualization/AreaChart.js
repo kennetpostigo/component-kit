8248856459007e225206f767ac7ae4b550c04ebf
Kennet Postigo <kennetfpostigo@gmail.com>
2016-06-14 13:09:06 -0400

    basic bar chart, need to add series and stack functionality.

--- src/visualization/AreaChart.js
+++ src/visualization/AreaChart.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import d3Shape from 'd3-shape';
+import d3Axis from 'd3-axis';
+import ReactFauxDOM from 'react-faux-dom';
 
 class AreaChart extends React.Component {
-  constructor (props, context) {
-    super(props, context);
-  }
-
   componentDidMount () {
 
   }
 
   render () {
+    const { width, height, data } = this.props;
 
+    const el = d3.select(ReactFauxDOM.createElement('svg'))
   }
-
 }
 
 export default AreaChart;
+
+AreaChart.proptypes = {
+  width: React.PropTypes.number,
+  height: React.PropTypes.number,
+  data: React.PropTypes.array
+}
--- src/visualization/BarChart.js
+++ src/visualization/BarChart.js
@@ -1,18 +1,55 @@
 import React from 'react';
+import d3 from 'd3';
+import ReactFauxDOM from 'react-faux-dom';
+import {StyleSheet, css} from 'aphrodite';
 
 class BarChart extends React.Component {
-  constructor (props, context) {
-    super(props, context);
-  }
-
-  componentDidMount () {
-
-  }
-
   render () {
+    var styles = StyleSheet.create(this.props.styles);
 
+    const { width, height, data } = this.props;
+
+    var y = d3.scale.linear()
+      .domain([0, d3.max(data)])
+      .range([height, 0]);
+
+    var chart = d3.select(ReactFauxDOM.createElement('svg'))
+      .attr("width", width)
+      .attr("height", height);
+
+    var barWidth = width / data.length;
+
+    var bar = chart.selectAll("g")
+      .data(data)
+      .enter().append("g")
+      .attr("transform", (d, i) => `translate(${i * barWidth}, 0)`);
+
+    bar.append("rect")
+      .attr("y", (d) => y(d))
+      .attr("height", (d) => height - y(d))
+      .attr("width", barWidth - this.props.barGap)
+      .attr('class', `${css(styles.rect)}`);
+
+    bar.append("text")
+      .attr("x", barWidth / 2)
+      .attr("y", (d) => y(d) + 10)
+      .attr("dy", ".75em")
+      .text((d) => d)
+      .attr('class', `${css(styles.text)}`);
+
+    return chart.node().toReact();
   }
+}
 
+
+
+
+BarChart.proptypes = {
+  width: React.PropTypes.number,
+  height: React.PropTypes.number,
+  data: React.PropTypes.array,
+  barGap: React.PropTypes.number,
+  barSize: React.PropTypes.number
 }
 
 export default BarChart;
