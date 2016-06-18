59908bfa4c836b645ff9fa9ccfda12be90dd41ac
Kennet Postigo <kennetfpostigo@gmail.com>
2016-06-18 19:30:09 -0400

    rough draft of radialBarChart and radarChart

--- src/visualization/RadarChart.js
+++ src/visualization/RadarChart.js
@@ -1,18 +1,93 @@
 import React from 'react';
+import d3 from 'd3';
+import ReactFauxDOM from 'react-faux-dom';
 
 class RadarChart extends React.Component {
-  constructor (props, context) {
-    super(props, context);
-  }
-
-  componentDidMount () {
-
-  }
-
   render () {
+    const {width, height, data} = this.props,
+          barHeight = height / 2 - 40;
 
+    var tickValues = [4,8,12,16,20];
+
+    var chart = d3.select(ReactFauxDOM.createElement('svg'))
+        .attr('class', 'RadarChart')
+        .attr('width', width)
+        .attr('height', height);
+
+    var g = chart.append('g')
+      .attr('transform', `translate(${width / 2}, ${height / 2} )`);
+
+    var numBars = data.data.length;
+
+    var radius = d3.scale.linear()
+        .domain([0,20])
+        .range([0, barHeight]);
+
+    var line = d3.svg.line.radial()
+      .interpolate('linear-closed')
+      .radius((d) => radius(d.count))
+      .angle((d,i) => (i * 2 * Math.PI / numBars));
+
+    var area = d3.svg.area.radial()
+      .interpolate(line.interpolate())
+      .innerRadius(radius(0))
+      .outerRadius(line.radius())
+      .angle(line.angle());
+
+    tickValues.sort((a,b) => b - a);
+
+    var tickPath = g.selectAll('.tickPath')
+        .data(tickValues).enter()
+      .append('path')
+        .attr('class', 'tickPath')
+        .attr('d', function(d) {
+          var tickArray = [], i;
+          for (i = 0; i < numBars; i++) tickArray.push({count : d});
+          return area(tickArray);
+        })
+        .style('fill', 'none')
+        .style('stroke', (d,i) => (i === 0) ? 'rgb(102, 102, 102)' : 'rgb(102, 102, 102)')
+        .style('stroke-width', (d,i) => (i === 0) ? '.8px' : '.5px');
+
+    var lines = g.selectAll('line')
+        .data(data.data)
+      .enter().append('g')
+        .attr('class', 'lines');
+
+    lines.append('line')
+      .attr('y2', - barHeight )
+      .style('stroke', '#5e5e5e')
+      .style('stroke-width', '.5px')
+      .attr('transform', (d, i) => `rotate(${(i * 360 / numBars)})`);
+
+    lines.append('text')
+      .attr('class', 'names')
+      .attr('x', (d, i) => (barHeight + 15) * Math.sin((i * 2 * Math.PI / numBars)))
+      .attr('y', (d, i) => -(barHeight + 15) * Math.cos((i * 2 * Math.PI / numBars)))
+      .attr('text-anchor', function(d,i) {
+      if (i === 0 || i === numBars / 2) {
+          return 'middle';
+        }else if (i <= numBars / 2) {
+          return 'begin';
+        }else {
+          return 'end';
+        }
+      })
+      .style('font-weight', 'bold')
+      .style('fill', 'rgb(171, 166, 166)')
+      .text((d) => d.skill);
+
+    var layer = g.selectAll('.layer')
+      .data([data]).enter()
+      .append('path')
+      .attr('class', 'layer')
+      .attr('d', (d) => area(d.data))
+      .attr('fill', 'rgba(88, 201, 185, 0.7)')
+      .attr('stroke', 'rgb(102, 102, 102)')
+      .attr('stroke-width', '0.5px');
+
+    return chart.node().toReact();
   }
-
 }
 
 export default RadarChart;
--- src/visualization/RadialBarChart.js
+++ src/visualization/RadialBarChart.js
@@ -1,16 +1,81 @@
 import React from 'react';
+import d3 from 'd3';
+import ReactFauxDOM from 'react-faux-dom';
 
 class RadialBarChart extends React.Component {
-  constructor (props, context) {
-    super(props, context);
-  }
-
-  componentDidMount () {
-
-  }
-
   render () {
+    const {width, height, data, colors} = this.props,
+    innerW = width - 0,
+    innerH = height - 40,
+    radiusMax = (height / 2.2),
+    xColumn = "name",
+    sliceSizeColumn = "population",
+    colorColumn = "religion";
 
+    var xScale = d3.scale.ordinal().rangePoints([0, width]);
+
+    var xAxis = d3.svg.axis().scale(xScale).orient("bottom")
+        .outerTickSize(0);
+
+    var colorScale = d3.scale.ordinal()
+        .range(colors);
+    var angleScale = d3.scale.linear();
+
+    var chart = d3.select(ReactFauxDOM.createElement('svg'))
+        .attr('class', 'RadialBarChart')
+        .attr('width', width)
+        .attr('height', height);
+
+    var g = chart.append("g")
+        .attr('class', 'plane')
+        .attr("transform", "translate(" + 11 + "," + 33 + ")");
+
+    var pieG = g.append("g");
+
+
+    var pie = d3.layout.pie();
+    var arc = d3.svg.arc();
+
+      xScale.domain(data.map((d) => d.name));
+
+      colorScale.domain(data.map((d) => d[colorColumn]));
+
+      pie.value((d) => d[sliceSizeColumn]);
+
+      angleScale.domain([0, d3.max(data, (d) => d[sliceSizeColumn])])
+        .range([0, Math.PI]);
+
+      var pieData = pie(data);
+      pieData.forEach(function (d){
+        d.startAngle = 0;
+        d.endAngle = angleScale(d.value);
+      });
+
+      pieG.attr("transform", `translate(${width / 2}, ${innerH}) rotate(-90)`)
+          .attr('class', `pieSlice`);
+
+      var slices = pieG.selectAll("path")
+          .data(pieData);
+
+      slices.append('text')
+          .attr('dy', '.35em')
+          .text((d) => d.population);
+
+      slices.enter().append("path")
+        .attr("d", function (d, i){
+          arc
+            .innerRadius(function(d) {
+              return i / (pieData.length - 1) * radiusMax;
+            })
+            .outerRadius(function(d) {
+              return (i + 1) / (pieData.length - 1) * radiusMax;
+            });
+          return arc(d);
+        })
+        .attr("fill", function (d){ return colorScale(d.data[colorColumn]); });
+      slices.exit().remove();
+
+    return chart.node().toReact();
   }
 
 }
