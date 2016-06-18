38d1368afbd5c67db73fcfcb09f445cfec7d0371
Kennet Postigo <kennetfpostigo@gmail.com>
2016-06-18 19:31:37 -0400

    removed commented out code.

--- src/visualization/ScatterPlot.js
+++ src/visualization/ScatterPlot.js
@@ -39,25 +39,6 @@ class ScatterPlot extends React.Component {
       .attr('cy', (d) => yScale(d[dataKey]))
       .style('fill', (d) => color(d[scatterKey]));
 
-    // var legend = g.selectAll('.legend')
-    //     .data(color.domain())
-    //     .enter().append('g')
-    //     .attr('class', 'legend')
-    //     .attr('transform', (d, i) => `translate(0, ${i * 20})`);
-    //
-    // legend.append('rect')
-    //     .attr('x', innerW - 18)
-    //     .attr('width', 18)
-    //     .attr('height', 18)
-    //     .style('fill', color);
-    //
-    // legend.append('text')
-    //     .attr('x', width - 24)
-    //     .attr('y', 9)
-    //     .attr('dy', '.35em')
-    //     .style('text-anchor', 'end')
-    //     .text((d) => d.t);
-
     return plane.node().toReact();
   }
 }
